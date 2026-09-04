89d7806e39ea0e017b0cdc204706b7d448823744
Meghana <meghanasomashekar7@gmail.com>
2026-09-04 17:07:27 +0530

    fix render port configuration

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const Message = require("./models/Message.js");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
